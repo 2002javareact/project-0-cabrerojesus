405af48c959489ce5b5464170e1a513f2dd0c98b
Jesus Cabrero <cabrerojesus@gmail.com>
2020-02-19 14:32:54 -0500

    project-0

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,76 @@
 
-import * as express from 'express'
+import * as express from 'express';
+import  * as bodyParser from 'body-parser';
 import { users } from './testDatabase';
-import bodyParser = require('body-parser');
-import { Users } from './models/Users';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { BadCredentialsError } from './errors/BadCredentialsError';
 
 
 const app = express()  
 
 app.use('/', bodyParser.json())
 
-app.get('/', (req,res)=>{
-    //get all of our users
-    //format them to json
-    //use the response obj to send them back
-    res.json(users)// this will format the object into json and send it back    
+//find and return all users
+//only permissable to admin roles
+app.get('/users',(req,res)=>{
+})
+
+//call the session middleware. 
+app.use(sessionMiddleware)
+
+app.post('/login', (req,res)=>{
+  //step one, get data from user
+  const {username, password} = req.body
+  //step two, validate that data
+  if(!username || !password){
+      res.sendStatus(400).send('Please Include Username and Password')
+  } 
+  else {
+      try {
+            const user = findByUsernameAndPassword(username, password)
+            req.session.user =  user
+            res.status(200).json(user)
+      } catch(e){
+          res.sendStatus(e.status).send(e.message)
+      }
+  }
 })
 
 
-
-app.post('/', (req,res)=>{
-  let {username, password} = req.body
-
-  if(username && password)
-  {
-    for(let i = 0; i<users.length; ++i)
+//this function returns a user object if credentials are valid
+//otherwise it will throw an error
+function findByUsernameAndPassword(username:string, password:string){
+  for(let i=0;i<users.length;++i){
+    if(username === users[i].username && password === users[i].password)
     {
-      if(users[i].username === username && users[i].password)
-      {
-         res.status(202).json(users[i])
-      }
+      return users[i]
     }
   }
-  else
-  {
-    res.status(400).send('Please enter a username and a password')
-  }    
+  throw new BadCredentialsError()
+}
+
+
+//submit reimbursement request
+app.post('/reimbursement',(req,res)=>{
+  const {rId, auth, amnt,  // not null
+    dateSub, dateRes, descr, resol, stat, typ} = req.body
+
+    if(rId && auth && amnt && dateSub && dateRes && descr
+      && resol && stat && typ)
+      {
+          if(rId === 0){
+                
+          }
+          else{
+                res.send('please set responseId to 0')
+          }
+      }
+      else{
+        res.status(400).send('Please set all required information.')
+      }
 })
 
+//listens to port 2020 for request
 app.listen(2020, ()=>{
     console.log('app has started on port 2020');
 })
