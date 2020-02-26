7a502acecf7431c8a7316032c6f76338ec42e04c
Jesus Cabrero <cabrerojesus@gmail.com>
2020-02-26 11:58:45 -0500

    Finished project0

--- src/routers/reimbursement-router.ts
+++ src/routers/reimbursement-router.ts
@@ -13,83 +13,103 @@ reimbursementRouter.use(loggingMiddleware)
 
 //this function searches reimbursements by status id
 reimbursementRouter.get('/status/:statusId',async (req,res)=>{
-try{
-    if(req.session.user.role.roleId === 3){
-           throw new TokenExpiredError()
+    if(!req.session.user){
+        res.status(401).send('Please log in.')
     }
-    else{    
-            const {statusId}=req.body
-            if(statusId < 3 && statusId > 0){
-            const reimbursement = await findReimbursementByStatus(statusId)
-            res.status(200).json(reimbursement)
+    else{
+        try{
+            if(req.session.user.role.roleId === 3){
+                throw new TokenExpiredError()
             }
-            else{
-                throw new Error('Enter a valid status number.')
+            else{    
+                    const {statusId}=req.body
+                    if(statusId < 3 && statusId > 0){
+                    const reimbursement = await findReimbursementByStatus(statusId)
+                    res.status(200).json(reimbursement)
+                    }
+                    else{
+                        res.status(400).send('Enter a valid status number.')
+                    }
             }
-        }
-    } 
+        } 
         catch(e){
-            res.status(e.status).send(e.message)
+               res.status(e.status).send(e.message)
         }
+    }
 })
 
 //this function searches by author
 reimbursementRouter.get('/author/:userId',async (req,res)=>{
+    if(!req.session.user){
+        res.status(401).send('Please log in.')
+    }
+    else{
     const{userId}=req.body
-    try{
-        if(req.session.user.role.roleId > 2 && req.session.user.userId != userId){
-            throw new TokenExpiredError()
-        }
-        else{
-                if(!isNaN(userId)){
-                const reimbursement = await findByReimbursementAuthor(userId)
-                res.status(200).json(reimbursement)
+            try{
+                if(req.session.user.role.roleId > 2 && req.session.user.userId != userId){
+                    throw new TokenExpiredError()
                 }
                 else{
-                    throw new Error("Enter a valid user id.")
-                }
-              }
+                        if(!isNaN(userId)){
+                        const reimbursement = await findByReimbursementAuthor(userId)
+                        res.status(200).json(reimbursement)
+                        }
+                        else{
+                            throw new Error("Enter a valid user id.")
+                        }
+                    }
             }
-            catch(e){
+             catch(e){
                 res.status(e.status).send(e.message)
-            }
+        }
+    }
 })
 
 //this function submits a new reimbursement
 reimbursementRouter.post('/',async (req,res)=>{
-        try{
+    if(!req.session.user){
+        res.status(401).send('Please log in.')
+    }
+    else{
+         try{
             const{amount,dateSubmitted,description,type}=req.body
             if(amount && dateSubmitted && description && type){
-                const reimbursement = await submitReimbursement(new ReimbursementDto(0,req.session.user.userId,amount,dateSubmitted,dateSubmitted,description,null,3,type))
-                res.status(201).json(reimbursement)
-            }
-            else{
-                throw new Error("Please provide all information required for reimbursement.")
-            }
-    }
-    catch(e){
-            res.status(e.status).send(e.message)
+                   const reimbursement = await submitReimbursement(new ReimbursementDto(0,req.session.user.userId,amount,dateSubmitted,19700101,description,null,3,type))
+                    res.status(201).json(reimbursement)
+                }
+                else{
+                    throw new Error("Please provide all information required for reimbursement.")
+                }
+            }   
+             catch(e){
+                res.status(e.status).send(e.message)
+        }
     }
 })
 
 //this function updates a reimbursement
 reimbursementRouter.patch('/users', async (req,res)=>{
-    const {reimbursementId,author,amount,dateSubmitted,dateResolved,description,resolver,status,type}=req.body
-    try{
-        if(!reimbursementId){
-            throw new Error('Please include a reimbursement id.')
-        }
-        else{
-                if(req.session.user.role.roleId <= 2){
-                const reimbursement = await updateReimbursement(new ReimbursementDto(reimbursementId,author,amount,dateSubmitted,dateResolved,description,resolver,status,type))
-                res.status(201).json(reimbursement)
-             }
-             else{
-                 throw new TokenExpiredError()
-             }
-        }
+    if(!req.session.user){
+        res.status(401).send('Please log in.')
     }
-    catch(e){
-        res.status(e.status).send(e.message)
+    else{
+        const {reimbursementId,author,amount,dateSubmitted,dateResolved,description,resolver,status,type}=req.body
+        try{
+            if(!reimbursementId){
+                throw new Error('Please include a reimbursement id.')
+            }
+            else{
+                    if(req.session.user.role.roleId <= 2){
+                    const reimbursement = await updateReimbursement(new ReimbursementDto(reimbursementId,author,amount,dateSubmitted,dateResolved,description,resolver,status,type))
+                    res.status(201).json(reimbursement)
+                }
+                else{
+                    throw new TokenExpiredError()
+                }
+            }
+        }
+        catch(e){
+            res.status(e.status).send(e.message)
+        }
     }
 })
--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -4,7 +4,7 @@ import { TokenExpiredError } from '../errors/TokenExpiredError';
 import { findByUsernameAndPassword, findByUserId, findAllUsers, updateUser } from '../services/user-service';
 import { loggingMiddleware } from '../middleware/loggin-middleware';
 import { UserDto } from '../dtos/UserDto';
-import { Role } from '../models/Role';
+
 
 
 export const userRouter = express.Router()
@@ -24,8 +24,8 @@ userRouter.use(loggingMiddleware)
     else {
         try {
               const user = await findByUsernameAndPassword(username, password)
-              req.session.user =  user
-              res.status(200).json(user)
+              req.session.user = user
+              res.status(200).json(user);
         } catch(e){
             res.status(e.status).send(e.message)
         }
@@ -37,44 +37,54 @@ userRouter.use(loggingMiddleware)
   //if user is financial manager will return all users
   //else will send error 
   userRouter.get('/',async (req,res)=>{
-    try{
-      if(req.session.user.role.roleId > 2){
-          throw new TokenExpiredError()
+    if(req.session.user){
+          try{
+            if(req.session.user.role.roleId > 2){
+                throw new TokenExpiredError()
+            }
+            else{
+              const users = await findAllUsers()
+              res.status(200).json(users)        
+            }
+        }//end of try
+        catch(e){
+          res.status(e.status).send(e.message)
+        }//end of catch
+      }else{
+        res.status(400).send('Please log in.')
       }
-      else{
-        const users = await findAllUsers()
-        res.status(200).json(users)        
-      }
-  }//end of try
-  catch(e){
-    res.status(e.status).send(e.message)
-  }//end of catch
   })
 
   //find and return user with the matching user id
   userRouter.get('/:id',async (req,res)=>{
-    const {userId}=req.body
-    if(isNaN(userId)){
-      res.status(400).send('Please enter a valid user Id.')
-    }
-    else{
-      try{
-        if(req.session.user.role.roleId <= 2 || userId === req.session.user.userId){          
-            const user = await findByUserId(userId)
-            res.status(200).json(user)
+    if(req.session.user){
+        const {userId}=req.body
+        if(isNaN(userId)){
+          res.status(400).send('Please enter a valid user Id.')
         }
-      else{
-        throw new TokenExpiredError()
+        else{
+          try{
+            if(req.session.user.role.roleId <= 2 || userId === req.session.user.userId){          
+                const user = await findByUserId(userId)
+                res.status(200).json(user)
+            }
+          else{
+            throw new TokenExpiredError()
+          }
+        }
+        catch(e){
+          res.status(e.status).send(e.message)
+        }
       }
     }
-    catch(e){
-      res.status(e.status).send(e.message)
-  }
+   else{
+         res.status(400).send('Please log in')
   }
 })
   
   //this function will update a user object
-  userRouter.patch('/',async (req,res)=>{
+ userRouter.patch('/',async (req,res)=>{
+  if(req.session.user){
     try{
       if(req.session.user.role.roleId === 1){
           const {userId,username,password,firstName,lastName,email,role} = req.body
@@ -93,7 +103,11 @@ userRouter.use(loggingMiddleware)
         catch(e){
           res.status(e.status).send(e.message)
         }
-      }) 
+      }
+      else{
+        res.status(400).send('Please log in')
+      }
+    }) 
 
 
   
