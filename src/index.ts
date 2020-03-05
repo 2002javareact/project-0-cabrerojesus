975f4a410096f53117d069852bfdd02e12720618
Jesus Cabrero <cabrerojesus@gmail.com>
2020-03-05 18:01:43 -0500

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import * as express from 'express';
 import  * as bodyParser from 'body-parser';
 import { userRouter } from './routers/user-router'
 import  { reimbursementRouter } from './routers/reimbursement-router'
-import {  CorsFilter } from './middleware/cors-filter';
+import {  corsFilter } from './middleware/cors-filter';
 
 const app = express()  
 
 
 app.use('/', bodyParser.json())
-app.use(CorsFilter)
+app.use(corsFilter)
 
 // this will register all http requests that match /users
 // and redirect them to the userRouter and the reimbursementRouter
